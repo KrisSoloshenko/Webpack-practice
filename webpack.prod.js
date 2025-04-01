6f43b3e560d6dbd512111e0bb69f4f08c821c8d5
KrisSoloshenko <dinislamovak@gmail.com>
2025-04-01 16:52:50 +0500

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js'
-    },
+    }
     plugins: [
         new MiniCssExtractPlugin(),
         new CssMinimizerWebpackPlugin(),
